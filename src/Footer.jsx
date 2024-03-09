dbd0339a0d1c71bab8b10723d79534b4d7da2f6c
Cristian B <cris.bahena77@gmail.com>
2024-03-09 12:04:10 -0600

    initial commit

---
+++ src/Footer.jsx
@@ -0,0 +1,7 @@
+export function Footer(){
+  return(
+    <div>
+      <h4>Copyright 2024</h4>
+    </div>
+  )
+}
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+export function Header(){
+  return(
+    <div>
+      <nav>
+        <a href="/">La Vida</a> | <a href="/">Home</a> | <a href="/">Recipes</a>
+      </nav>
+    </div>
+  )
+}

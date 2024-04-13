035d012692d0dbd09348bde7bb9dabe3af32bdc2
Cristian B <cris.bahena77@gmail.com>
2024-04-12 23:38:45 -0500

    added cards to recipes index

--- src/RecipesIndex.jsx
+++ src/RecipesIndex.jsx
@@ -1,21 +1,37 @@
 /* eslint-disable react/prop-types */
+import { Card, Col, Row, Button, Flex } from 'antd';
+
+const { Meta } = Card;
+
+
 export function RecipesIndex(props){
 
+  // console.log(props.recipe.name)
   return(
-    <div>
+    <div>   
       <h1>All Recipes</h1>
       {props.recipes.map((recipe)=>(
         <div key={recipe.id}>
-          <h2>Id:{recipe.id}</h2>
-          <h2>{recipe.name}</h2>
-          <img src={recipe.image} />
-          <p>Description: {recipe.description}</p>
-          <p>Ingredients:{recipe.ingredients}</p>
-          <p>Instructions:{recipe.instructions}</p>
-          <p>Time:{recipe.time}</p>
-          <p>Creator:{recipe.user_id}</p>
-          <p>Servings:{recipe.servings}</p>
-          <button onClick={()=>props.displayRecipe(recipe)}> More Info </button>
+          <Row gutter={16}>
+            <Col span={8}>
+              <Card
+                hoverable
+                style={{
+                  width: 500,
+                }}
+                cover={<img alt={recipe.name} src={recipe.image} onClick={()=>props.displayRecipe(recipe)} />}
+              >
+                <Meta title={recipe.name}/>
+                <br />
+                {recipe.description}
+                <br />
+                Time: {recipe.time} <br />
+                Servings: {recipe.servings}
+                
+              </Card>
+            </Col>
+          </Row>
+          <br />
         </div>
       ))}
     </div>

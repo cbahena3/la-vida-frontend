4a047d003c2d70cb875935a6fd3bed66f1a6be1f
Cristian B <cris.bahena77@gmail.com>
2024-04-16 11:36:21 -0500

    added icons to footer with links

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,28 @@
+import {
+  LinkedinOutlined,
+  InstagramOutlined,
+  MailOutlined,
+} from '@ant-design/icons';
+import { Space } from 'antd';
+
+
 export function Footer(){
   return(
     <div>
-      <h4>Copyright 2024</h4>
+      <h4>
+        <Space>
+          Copyright 2024
+          <a href="https://www.linkedin.com/in/cristianbahena/">
+            <LinkedinOutlined/>
+          </a>
+          <a href="https://www.instagram.com/cristianbahen/?hl=en">
+            <InstagramOutlined />
+          </a>
+          <a href="mailto:cris.bahena77@gmail.com">
+            <MailOutlined />
+          </a>
+        </Space>
+      </h4>
     </div>
   )
 }
